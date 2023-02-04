34942181a71f7313e76c0ed9aec49d875b56a0b4
Kyle Clemente <kclemente11@me.com>
2023-02-03 19:04:34 -0600

    React-auth revamped

--- react-auth/src/App.js
+++ react-auth/src/App.js
@@ -1,17 +1,34 @@
-import Register from './components/Register'
-import Login from './components/Login'
-import { Routes, Route, Switch } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+import { Container, Col, Row } from "react-bootstrap";
+import Account from "./Account";
+import FreeComponent from "./FreeComponent";
+import AuthComponent from "./AuthComponent";
+import PrivateRoutes from "./PrivateRoutes"
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path='' component={<Register />} >
-        <Route index element={<Register />} />
-        <Route path='/login' element={<Login />} />
-      </Route>
-    </Routes>
-    </>
+    <Container>
+      <Row>
+        <Col className="text-center">
+          <h1>MaintainMe</h1>
+
+          <section id="navigation">
+            <a href="/">Home</a>
+            <a href="/free">Free Component</a>
+            <a href="/auth">Auth Component</a>
+          </section>
+        </Col>
+      </Row>
+
+      {/* create routes here */}
+      <Routes>
+        <Route exact path="/" element={<Account/>} />
+        <Route exact path="/free" element={<FreeComponent/>} />
+        <Route element={<PrivateRoutes/>}> 
+          <Route path="/auth" element={<AuthComponent/>}/>
+        </Route>
+      </Routes>
+    </Container>
   );
 }
 
